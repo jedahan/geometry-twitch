858201b64cece9b3c1da8f74ad25462d6856cf32
Jonathan Dahan <hi@jonathan.is>
2018-10-16 10:04:27 -0400

    port to geometry:mnml

---
+++ geometry_todo.zsh
@@ -0,0 +1,24 @@
+: ${GEOMETRY_TODO_COLOR:=gray}
+: ${GEOMETRY_TODO_FILE:=${HOME}/.todo.md}
+
+touch $GEOMETRY_TODO_FILE
+
+todo_head() {
+  head -n1 $GEOMETRY_TODO_FILE
+}
+
+function geometry_todo() {
+  local top=$(todo_head)
+  test -n $top || return
+  echo -n $(color $GEOMETRY_TODO_COLOR $top)
+}
+
+todo() {
+  echo $* >> $GEOMETRY_TODO_FILE
+}
+
+todone() {
+  local finished=$(todo_head)
+  sed -ie '1d' $GEOMETRY_TODO_FILE
+  echo finished $finished
+}
