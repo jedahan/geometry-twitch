ca647d42d195859aee44944bc4ac14989406f60a
Jonathan Dahan <hi@jonathan.is>
2019-02-08 11:31:39 -0500

    fix empty return

--- geometry_todo.zsh
+++ geometry_todo.zsh
@@ -6,15 +6,15 @@ touch $GEOMETRY_TODO $GEOMETRY_TODONE
 
 geometry_todo() {
   todo=$(head -n1 $GEOMETRY_TODO 2>/dev/null)
-  [[ -z todo ]] && return
-  ansi ${GEOMETRY_TODO_COLOR:=gray} $(head -n1 $GEOMETRY_TODO)
+  [[ -z "$todo" ]] && return
+  ansi ${GEOMETRY_TODO_COLOR:=gray} $todo
 }
 
 todo() { echo $* >> $GEOMETRY_TODO }
 
 todone() {
   finished=$(head -n1 $GEOMETRY_TODO 2>/dev/null)
-  [[ -z $finished ]] && return
+  [[ -z "$finished" ]] && return
   \sed -ie '1d' $GEOMETRY_TODO
   echo $finished >> $GEOMETRY_TODONE
   echo finished $finished
